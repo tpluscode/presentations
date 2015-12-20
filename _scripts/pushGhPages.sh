b4c9667181a3fd3d4bc350cdf65d9dd0031c7627
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2015-12-20 20:59:27 +0100

    Update pushGhPages.sh

--- _scripts/pushGhPages.sh
+++ _scripts/pushGhPages.sh
@@ -5,5 +5,5 @@ ls web-components
 git add -Af .
 if [ "git status --porcelain" ]; then
     git commit -am "pushing presentation built on travis (build number ${TRAVIS_BUILD_NUMBER})";
-    git push https://${GH_OAUTH_TOKEN}@github.com/tpluscode/presentations gh-pages  > /dev/null 2>&1;
+    git push https://${GH_OAUTH_TOKEN}@github.com/tpluscode/presentations gh-pages;
 fi
